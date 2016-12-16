888bdf0438b63c63113be77c7adb174d27ab5c10
poojajadhav21 <pooja.jadhaveshwarlal1@marist.edu>
2016-12-15 23:47:42 -0500

    final exam

---
+++ final/finalGenData.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+#generates sinc function with noise 
+def genDataSet(N):
+    x = np.sort(np.random.normal(0, 1, N)*6)
+    ytrue = np.sinc(x)
+    noise = np.random.normal(0, 0.2, N)
+    y = ytrue + noise
+    X = np.c_[x, y]
+    return X, ytrue
